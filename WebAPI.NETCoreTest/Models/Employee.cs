8d3c12882a2aaf766e9fc8e0cacb1e338449fb7b
Kristanto Nugraha <knugroho150991@live.com>
2020-09-02 16:58:07 +0700

    Initial project commit

---
+++ WebAPI.NETCoreTest/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.NETCoreTest.Models
+{
+    public class Employee
+    {
+        public int NIK { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public DateTime Birthdate { get; set; }
+        public string Department { get; set; }
+        public string Roles { get; set; }
+
+    }
+}
---
+++ WebAPI.NETCoreTest/Models/UserID.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.NETCoreTest.Models
+{
+    public class UserID
+    {
+        public string user_ID { get; set; }
+
+        public string password { get; set; }
+
+        public string name { get; set; }
+    }
+}
